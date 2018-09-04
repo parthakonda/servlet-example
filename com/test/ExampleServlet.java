73b1359c7c560449e01b73041cbb45c9a21d1d85
parthz <parthsaradhi1992@gmail.com>
2018-09-04 23:00:34 +0530

    Initial Setup

---
+++ com/test/ExampleServlet.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class ExampleServlet extends HttpServlet {
+
+    // Some data
+    private String data = "My Static Data";
+
+    public void init() throws ServletException {
+        // Settings message
+        System.out.println("in Init");
+        data = "Changed message";
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        System.out.println("in Get");
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>" + data + "</h1");
+    }
+
+    public void destroy() {
+        System.out.println("I'm destroyed");
+    }
+}
