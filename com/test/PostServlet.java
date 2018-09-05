9d77c4af8d5f1d77d9b53d2c5935dba4f4de23d6
parthz <parthsaradhi1992@gmail.com>
2018-09-05 13:28:26 +0530

    Updated with service methods

---
+++ com/test/PostServlet.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+public class PostServlet extends HttpServlet {
+
+    public void init() throws ServletException{
+        System.out.println("Servlet initiate");
+    }
+
+
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Read the parameters
+        Integer value1 = Integer.parseInt(request.getParameter("value1"));
+        Integer value2 = Integer.parseInt(request.getParameter("value2"));
+
+        // Do some stuff
+        Integer results = value1 + value2;
+
+        // Return response
+        PrintWriter out = response.getWriter();
+        out.println("Results:" + results);
+
+    }
+
+    public void destroy() {
+        System.out.println("Servlet Destroyed");
+    }
+}
